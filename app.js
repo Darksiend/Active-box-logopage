01555590a268a535bd81503b77add5e49aa4a687
Anton <logosmk2@gmail.com>
2019-08-29 02:02:10 +0300

    Add burger

--- app.js
+++ app.js
@@ -1,23 +1,53 @@
 $(function () {
     let header = $("#header");
     let intro = $("#intro");
-    let introH;
+    let introH = intro.innerHeight();
     let scrollPos = $(window).scrollTop();
-
-    $(window).on("scroll load resize", function () {
+    checkScroll(scrollPos, introH)
+    $(window).on("scroll  resize", function () {
         introH = intro.innerHeight();
         scrollPos = $(this).scrollTop();
 
+        checkScroll(scrollPos, introH);
+
+
+    });
+
+    function checkScroll() {
         if (scrollPos > introH) {
             header.addClass("fixed");
         } else {
             header.removeClass("fixed");
 
         }
+    }
+
+
+    $("[data-scroll]").on("click", function (event) {
+        event.preventDefault();
+
+        let elementID = $(this).data('scroll');
+        let elementOffset = $(elementID).offset().top;
+        nav.removeClass("show");
+
+        $("html,body").animate({
+            scrollTop: elementOffset - 70
+        }, 700);
+
+
+
+    });
+
+    let nav = $("#nav");
+    $('#navToggle').on("click", function (event) {
+        event.preventDefault();
+
+        nav.toggleClass("show");
+
+
 
 
     });
 
 
-    header.addClass("fixed");
 });
